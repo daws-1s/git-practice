62ef41afa0b773cbdfed4dc547713ffa680b9d08
divya kamatham <divyakamatham12@gmai.com>
2024-08-24 20:11:03 +0800

    02-conversation.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
-
-
+ctrl+s --> save , this is onlyfor information purpose.not part of code
+# Author - Divya kamatham
+# Team - DevOps
 echo "Hello World"
 
 
+ 
--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Ramesh:: Hi Suresh, How are you?"
-echo "Suresh:: Hello Ramesh.I amfine."
-echo "Ramesh:: I am doing good Suresh."
+echo "Suresh:: Hello Ramesh.I am fine.How are you doing?"
+echo "Ramesh:: I am doing good Suresh.What's going on?"
 echo "suresh:: I started learning shell sript Ramesh."
