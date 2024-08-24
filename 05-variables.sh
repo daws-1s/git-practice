395f4f8815d5d134280b8c0e89c07aa91aaf636a
divya kamatham <divyakamatham12@gmai.com>
2024-08-25 00:50:51 +0800

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME #takes input into USERNAME variables
+
+echo "username entered is: $ USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+echo "password entered is: $ PASSWORD"
