d613625eac6e3a7de79cb3b048ac4a5ed7425d2c
divya kamatham <divyakamatham12@gmai.com>
2024-08-24 17:50:27 +0800

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh.I amfine."
+echo "Ramesh:: I am doing good Suresh."
+echo "suresh:: I started learning shell sript Ramesh."
