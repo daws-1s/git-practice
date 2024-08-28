424d46f7f6d75fd2a8c816dbc9d2fcc836ed37a5
divya kamatham <divyakamatham12@gmai.com>
2024-08-28 16:01:08 +0800

    functions

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+   echo $id
+   done
 # for i in {0..1000} #0-1000
 # do
 # 	echo $i
 # done
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
-echo "9"
-echo "10"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
