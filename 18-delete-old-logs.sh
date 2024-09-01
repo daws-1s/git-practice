1e28cdd90840f5959704ec247c4c6aa809fe1d3c
divya kamatham <divyakamatham12@gmai.com>
2024-09-01 20:43:37 +0800

    shell

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -12,7 +12,14 @@ then
 else
     echo -e "$SOURCE_DIR $R does not exist $N"
     exit 1
-    fi
+fi
 
-    FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-    echo "Files: $FILES"
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+# dont use line, it is reserverd word
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+    echo "Deleting file: $file"
+    rm -rf $file
+done <<< $FILES
